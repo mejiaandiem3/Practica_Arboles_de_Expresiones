aa4a676d573a38ee46216b4c8cdb812aae53d475
Andrea Evelyn Mejía Rubio <69946882+mejiaandiem3@users.noreply.github.com>
2022-09-01 13:16:36 -0500

    Agregar archivos de proyecto.

---
+++ Practica_Arboles_de_Expresiones/Arboles.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Practica_Arboles_de_Expresiones
+{
+    static class Arboles
+    {
+        static public bool convert(string infix) {
+            List<char> operadores = new List<char> { '*', '/', '+', '-' };
+
+            int pri = 0;
+            string[] result = { infix, "" };
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char item in infix.ToCharArray())
+            {
+                if (operadores.Contains(item))
+                    stack.Push(item);
+            }
+
+
+            return false;
+
+            
+        
+        }
+    }
+}
